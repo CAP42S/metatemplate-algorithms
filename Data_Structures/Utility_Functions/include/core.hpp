c10463b1abc94c901c2c76c3fc3f85e67b368090
Samer W Ali <108325733+CAP42S@users.noreply.github.com>
2023-07-26 17:13:53 -0700

    added custom memory allocation functions

--- Data_Structures/Utility_Functions/include/core.hpp
+++ Data_Structures/Utility_Functions/include/core.hpp
@@ -2,7 +2,21 @@
 #define CUTE_CORE_HPP
 namespace cute {
     long long ceil(unsigned long long n, long long divisor)
-        {return n/divisor + bool(n % divisor)}
+        {return n/divisor + bool(n % divisor);}
 
+    template<typename T>
+    T* rawMemory(int n)
+        {return (T*)::operator new(sizeof(T) * n);}
+
+    void rawDelete(void* array)
+        {::operator delete(array);}
+
+    template<typename T>
+    void rawDestruct(T* array, int size)
+    {
+        for(int i = 0; i < size; i++)
+            array[i].~T();
+        rawDelete(array);
+    }
 }
 #endif CUTE_CORE_HPP
