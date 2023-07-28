c3acb4576bab59dc29a3bdacc70f9b03c6bd0eca
Samer W Ali <108325733+CAP42S@users.noreply.github.com>
2023-07-27 18:15:48 -0700

    fixed custom semantics

--- Data_Structures/Utility_Functions/include/core.hpp
+++ Data_Structures/Utility_Functions/include/core.hpp
@@ -1,10 +1,24 @@
+/*
+ * @author: Samer Ali
+ * @desc: utility toolkit extension library for C++ std utility lib
+ */
 #ifndef CUTE_CORE_HPP
 #define CUTE_CORE_HPP
 #include <cstdint>
+#include <utility>
+#include <type_traits>
+#include <algorithm>
 namespace cute {
 
     //empty struct. used for template types that are not necessary
     struct empty {};
+
+    template<typename T>
+    using has_assign_op = decltype(std::declval<T&>() = std::declval<T const&>());
+
+    template<typename T>
+    struct has_assign_operator : std::integral_constant<bool,
+                                                    !std::is_void<has_assign_op<T>>::value> {};
 
     int64_t ceil(long long n)
     {
@@ -20,9 +34,14 @@ namespace cute {
 
 
     template<typename T>
-    T& genericAssign(T& to, T const& rhs)
+    std::enable_if_t<!has_assign_op<T>::value, T&>
+    T& genericAssign(T& to, T&& rhs)
     {
-
+        auto old = std::exchange(to, std::forward<T>(rhs));
+        if(&old != &rhs)
+        {
+            std::exchange(to,old);
+        }
     }
 }
 #endif CUTE_CORE_HPP
